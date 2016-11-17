efa14f045f5e7f25974038e5bd17bf878ba405b1
Damian Senn <senn@puzzle.ch>
2016-11-17 09:22:13 +0100

    Setup component css

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,33 @@
-/*jshint node:true*/
-/* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+/* eslint-env node */
+'use strict'
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app')
+const autoprefixer = require('autoprefixer')
+const cssnext = require('postcss-cssnext')
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
-  });
+  let app = new EmberApp(defaults, {
+    postcssOptions: {
+      compile: {
+        enabled: false
+      },
+      filter: {
+        enabled: true,
+        plugins: [
+          {
+            module: cssnext,
+            options: {
+              browsers: [
+                '> 1%',
+                'last 3 versions',
+                'Firefox ESR'
+              ]
+            }
+          }
+        ]
+      }
+    }
+  })
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-
-  return app.toTree();
-};
+  return app.toTree()
+}
