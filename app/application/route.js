12e74273b0fe718bb2a64ff365707f9c72764efe
Damian Senn <damian.senn@topaxi.codes>
2016-11-19 17:05:00 +0100

    Minor application route cleanup

--- app/application/route.js
+++ app/application/route.js
@@ -1,20 +1,19 @@
 import Ember from 'ember'
-import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin'
+import ApplicationRouteMixin
+  from 'ember-simple-auth/mixins/application-route-mixin'
 
-const { Route, inject } = Ember
+const { Route } = Ember
 
 export default Route.extend(ApplicationRouteMixin, {
-  session: inject.service(),
-
   actions: {
     loginWithGoogle() {
-      return this.get('session').authenticate('authenticator:torii', 'google')
+      return this.session.authenticate('authenticator:torii', 'google')
     },
     loginWithGithub() {
-      return this.get('session').authenticate('authenticator:torii', 'github')
+      return this.session.authenticate('authenticator:torii', 'github')
     },
     logout() {
-      return this.get('session').invalidate()
+      return this.session.invalidate()
     }
   }
 })
