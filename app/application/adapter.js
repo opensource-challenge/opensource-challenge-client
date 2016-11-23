8c7cdb82c300ca543ab721eba7216784fa4de047
Damian Senn <damian.senn@topaxi.codes>
2016-11-23 10:31:39 +0100

    Register authorizer on adapter

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,7 +1,10 @@
 import DS from 'ember-data'
+import DataAdapterMixin
+  from 'ember-simple-auth/mixins/data-adapter-mixin'
 
 const { JSONAPIAdapter } = DS
 
-export default JSONAPIAdapter.extend({
-  namespace: 'api/v1'
+export default JSONAPIAdapter.extend(DataAdapterMixin, {
+  namespace: 'api/v1',
+  authorizer: 'authorizer:osc'
 })
