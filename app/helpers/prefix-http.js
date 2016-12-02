510bf3050d4ff74d2debd994b6c1c12d45ee6927
Stefan Heinemann <stefan.heinemann@codedump.ch>
2016-12-02 10:55:06 +0100

    prefixHttp empty url bugfix
    Handle empty urls passed to the prefixHttp helper

--- app/helpers/prefix-http.js
+++ app/helpers/prefix-http.js
@@ -1,6 +1,9 @@
 import Ember from 'ember'
 
 export function prefixHttp([ url ]) {
+  if (url === undefined) {
+    return ''
+  }
   return url.indexOf('http') === 0 ? url : `http://${url}`
 }
 
--- tests/unit/helpers/prefix-http-test.js
+++ tests/unit/helpers/prefix-http-test.js
@@ -5,18 +5,23 @@ import { prefixHttp } from 'opensource-challenge-client/helpers/prefix-http';
 
 describe('Unit | Helper | prefix http', function() {
   it('adds http://', function() {
-    let result = prefixHttp([ 'www.puzzle.ch' ]);
+    let result = prefixHttp([ 'www.puzzle.ch' ])
     expect(result).to.equal('http://www.puzzle.ch')
   })
 
   it('doesn\' add http:// when it is already in the url', function() {
-    let result = prefixHttp([ 'http://www.puzzle.ch' ]);
+    let result = prefixHttp([ 'http://www.puzzle.ch' ])
     expect(result).to.equal('http://www.puzzle.ch')
   })
 
   it('doesn\' add http:// when https:// is in the url', function() {
-    let result = prefixHttp([ 'https://www.puzzle.ch' ]);
+    let result = prefixHttp([ 'https://www.puzzle.ch' ])
     expect(result).to.equal('https://www.puzzle.ch')
+  })
+
+  it('can handle empty urls', function() {
+    let result = prefixHttp([ ]);
+    expect(result).to.equal('')
   })
 })
 
