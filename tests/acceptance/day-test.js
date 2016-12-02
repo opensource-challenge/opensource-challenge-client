24291d6e1975c3aabf242c0f24cc4402524a5ea3
Stefan Heinemann <stefan.heinemann@codedump.ch>
2016-12-02 10:56:13 +0100

    Prefix the urls in the day view as well

--- tests/acceptance/day-test.js
+++ tests/acceptance/day-test.js
@@ -1,8 +1,11 @@
+import Ember from 'ember'
 import { describe, it, beforeEach, afterEach } from 'mocha'
 import { expect } from 'chai'
 import startApp from '../helpers/start-app'
 import destroyApp from '../helpers/destroy-app'
 import { percySnapshot } from 'ember-percy'
+
+const { $ } = Ember
 
 describe('Acceptance | day', function() {
   let application
@@ -23,5 +26,13 @@ describe('Acceptance | day', function() {
     percySnapshot('/day/2015-12-06')
 
     expect(currentURL()).to.equal('/day/2015-12-06')
+  })
+
+  it('renders URLs correctly', async function() {
+    server.loadFixtures('challenges', 'users', 'contributions')
+
+    await visit('/day/2015-12-01')
+
+    expect($('.avatar-wrapper[href^=http]').length).to.equal(10)
   })
 })
