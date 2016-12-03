4fe48921414c8ba9388e52e4a652aaf7bc3ff786
Damian Senn <senn@puzzle.ch>
2016-12-03 09:27:29 +0100

    Add post contribution visual regression test

---
+++ tests/acceptance/post-contribution-test.js
@@ -0,0 +1,47 @@
+import { describe, it, beforeEach, afterEach } from 'mocha'
+import { expect } from 'chai'
+import startApp from '../helpers/start-app'
+import destroyApp from '../helpers/destroy-app'
+import { authenticateSession, invalidateSession }
+  from 'opensource-challenge-client/tests/helpers/ember-simple-auth'
+import { percySnapshot } from 'ember-percy'
+
+describe('Acceptance | post contribution', function() {
+  let application
+
+  beforeEach(function() {
+    application = startApp()
+  })
+
+  afterEach(function() {
+    destroyApp(application)
+  })
+
+  it('can redirects to login', async function() {
+    await visit('/post-contribution')
+
+    expect(currentURL()).to.equal('/login')
+  })
+
+  describe('Logged in', function() {
+    beforeEach(async  function() {
+      server.loadFixtures('users')
+
+      await authenticateSession(application, {
+        access_token: 'userid:1'
+      })
+    })
+
+    afterEach(async function() {
+      await invalidateSession(application)
+    })
+
+    it('can visit /post-contribution', async function() {
+      await visit('/post-contribution')
+
+      percySnapshot('/post-contribution')
+
+      expect(currentURL()).to.equal('/post-contribution')
+    })
+  })
+})
