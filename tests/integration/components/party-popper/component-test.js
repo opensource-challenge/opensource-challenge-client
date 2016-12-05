2c2feba26a5c031ce0d060e7e017440617e29330
Joshua Schär <schaer@puzzle.ch>
2016-12-05 13:50:05 +0100

    create party-popper component

---
+++ tests/integration/components/party-popper/component-test.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import { describeComponent, it } from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+
+describeComponent('party-popper', 'Integration | Component | party popper',
+  {
+    integration: true
+  },
+  function() {
+    it('renders', function() {
+      // Set any properties with this.set('myProperty', 'value');
+      // Handle any actions with this.on('myAction', function(val) { ... });
+      // Template block usage:
+      // this.render(hbs`
+      //   {{#party-popper}}
+      //     template content
+      //   {{/party-popper}}
+      // `);
+
+      this.render(hbs`{{party-popper}}`);
+      expect(this.$()).to.have.length(1);
+    });
+  }
+);
