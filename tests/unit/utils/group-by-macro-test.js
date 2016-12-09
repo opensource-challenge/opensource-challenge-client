f48c98467264e322b8c0cc88f2b317ac7d62ded7
Damian Senn <senn@puzzle.ch>
2016-12-09 15:20:33 +0100

    Minimal group-by-macro unit test

--- tests/unit/utils/group-by-macro-test.js
+++ tests/unit/utils/group-by-macro-test.js
@@ -1,11 +1,49 @@
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
-import groupByMacro from 'opensource-challenge-client/utils/group-by-macro';
+import Ember from 'ember'
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+import groupByMacro from 'opensource-challenge-client/utils/group-by-macro'
 
 describe('Unit | Utility | group by macro', function() {
-  // Replace this with your real tests.
   it('works', function() {
-    let result = groupByMacro();
-    expect(result).to.be.ok;
-  });
-});
+    let Obj = Ember.Object.extend({
+      grouped: groupByMacro('items', 'date')
+    })
+    let obj = Obj.create({
+      items: [
+        { id: 1, date: '2016-12-01' },
+        { id: 2, date: '2016-12-01' },
+        { id: 3, date: '2016-12-02' },
+        { id: 4, date: '2016-12-02' },
+        { id: 5, date: '2016-12-03' },
+        { id: 6, date: '2016-12-03' }
+      ]
+    })
+
+    expect(obj.get('grouped')).to.deep.equal([
+      {
+        property: 'date',
+        value: '2016-12-01',
+        items: [
+          { id: 1, date: '2016-12-01' },
+          { id: 2, date: '2016-12-01' },
+        ]
+      },
+      {
+        property: 'date',
+        value: '2016-12-02',
+        items: [
+          { id: 3, date: '2016-12-02' },
+          { id: 4, date: '2016-12-02' }
+        ]
+      },
+      {
+        property: 'date',
+        value: '2016-12-03',
+        items: [
+          { id: 5, date: '2016-12-03' },
+          { id: 6, date: '2016-12-03' }
+        ]
+      }
+    ])
+  })
+})
