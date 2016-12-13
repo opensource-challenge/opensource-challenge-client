27cb683831723aeab8c0e3a8c70c78bb4ce985ca
Damian Senn <senn@puzzle.ch>
2016-12-13 18:29:22 +0100

    Add challenge ref to contributions

--- app/models/contribution.js
+++ app/models/contribution.js
@@ -4,6 +4,7 @@ const { Model, attr, belongsTo } = DS
 
 export default Model.extend({
   user: belongsTo('user'),
+  challenge: belongsTo('challenge'),
   title: attr('string'),
   date: attr('string'),
   link: attr('string'),
