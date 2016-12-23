8de902e36e302e2e66dfe1ec6e053224f49b9a30
Damian Senn <senn@puzzle.ch>
2016-12-23 21:39:32 +0100

    Let ember data timeout after 10sec

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -4,11 +4,16 @@ import DataAdapterMixin
   from 'ember-simple-auth/mixins/data-adapter-mixin'
 
 const { JSONAPIAdapter } = DS
+const TIMEOUT = 10000
 
 export default JSONAPIAdapter.extend(DataAdapterMixin, {
   host: ENV.APP.host,
   namespace: 'api/v1',
   authorizer: 'authorizer:osc',
+
+  ajaxOptions(url, type, options) {
+    return Object.assign(this._super(url, type, options), { timeout: TIMEOUT })
+  },
 
   urlForCreateRecord(modelName/*, snapshot*/) {
     switch (modelName) {
