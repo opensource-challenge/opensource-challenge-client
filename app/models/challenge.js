5cb73771719fd29115f6aa9e7c73ccccac93f23e
Damian Senn <senn@puzzle.ch>
2017-10-10 15:05:12 +0200

    Hide add contribution button if challenge is not active yet

--- app/models/challenge.js
+++ app/models/challenge.js
@@ -1,10 +1,17 @@
+import Ember from 'ember'
 import DS from 'ember-data'
 
+const { computed } = Ember
 const { Model, attr, hasMany } = DS
 
 export default Model.extend({
   contributions: hasMany('contribution'),
   name: attr('string'),
   startsOn: attr('date'),
-  endsOn: attr('date')
+  endsOn: attr('date'),
+  closed: attr('boolean'),
+
+  active: computed('startsOn', function() {
+    return Date.now() >= this.get('startsOn')
+  }).volatile(),
 })
