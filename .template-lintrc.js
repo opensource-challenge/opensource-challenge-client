383f501d814fc0328e11f55bf508795faf546713
Damian Senn <damian.senn@gmail.com>
2019-05-29 21:16:46 +0200

    No implicit this in templates
    In preperation of ember octane

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -4,5 +4,14 @@ module.exports = {
   extends: 'recommended',
   rules: {
     'attribute-indentation': false,
+    'no-implicit-this': {
+      allow: [
+        'party-popper',
+        'service-worker-update-notify',
+        'latest-contribution',
+        'contribution-bubbles',
+        'contribution-calendar',
+      ],
+    },
   },
 }
