22ab9ee92462ea91909bf560a48d86c4bf9cad6e
Damian Senn <damian.senn@gmail.com>
2019-05-29 20:26:19 +0200

    For some reason, relative links in engines broke 🤷‍♂️

--- app/app.js
+++ app/app.js
@@ -1,29 +1,28 @@
 import RSVP from 'rsvp'
 import Application from '@ember/application'
-import { computed } from '@ember/object'
 import Resolver from './resolver'
 import loadInitializers from 'ember-load-initializers'
 import config from './config/environment'
 
-const { Promise } = RSVP
-
 window.NativePromise = window.Promise
-window.Promise = Promise
+window.Promise = RSVP.Promise
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver,
-  engines: computed(() => ({
-    admin: {
-      dependencies: {
-        externalRoutes: {
-          index: 'index',
+  get engines() {
+    return {
+      admin: {
+        dependencies: {
+          externalRoutes: {
+            index: 'index',
+          },
+          services: ['store', 'session', 'current-user'],
         },
-        services: ['store', 'session', 'current-user'],
       },
-    },
-  })),
+    }
+  },
 })
 
 loadInitializers(App, config.modulePrefix)
